2a2fbd4e3a07255648816fcd24752da5192f78af
chensi960515 <909492683@qq.com>
2022-07-14 15:41:54 +0800

    整合sec

--- pcs_package/SEC_options.py
+++ pcs_package/SEC_options.py
@@ -27,9 +27,9 @@ sec_token = PCS_getToken.Get_Token()
 token = sec_token.get_Token(url_sec_token)
 sec_cancelMeeting_url = conf['parameter']['url_sec_cancelMeeting']
 sec_startMeeting_url = conf['parameter']['url_sec_startMeeting']
+sec_closeMeeting_url = conf['parameter']['url_sec_colseMeeting']
+sec_cookie = conf['parameter']['sec_cookie']
 sec_path = '../eph_data/sec.txt'
-
-
 
 
 def get_sec_parameter(file_path, start_index, end_index):
@@ -60,7 +60,6 @@ def cancelMeeting():
         logging.logger.info(response.text)
 
 
-
 def startMeeting():
     """
     开启 畅听会议
@@ -79,23 +78,33 @@ def startMeeting():
                     phoneNumber_list.append(i.replace("\'", "").strip())
                 payload = json.dumps({
                     "hostPasscode": hostPasscode,
-                    "hostCode": phoneNumber_list
+                    "hostCode": phoneNumber_list[0:1],
+                    "partyCode": phoneNumber_list[1:]
                 })
                 logging.logger.info(payload)
-                headers = {"content-type": "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW"}
+                headers = {"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36", "Accept":"*/*", "Accept-Encoding":"gzip, deflate, br","Connection": "keep-alive", "cookie": sec_cookie}
 
-                response = requests.request("POST", url, headers=headers, data=payload)
+                response = requests.request("POST", url, headers=headers, data=payload, files=[])
                 logging.logger.info(response.text)
 
 
+def closeMeeting():
+    """
+    结束 畅听会议
+    :return:
+    """
+    hostPasscodes = get_sec_parameter('../eph_data/sec.txt', 13, 22)
+    url = sec_closeMeeting_url
+    headers = {'Content-Type': 'application/json',"Cookie": sec_cookie}
+    for hostPasscode in hostPasscodes:
+        payload = json.dumps({
+            "hostPasscode": hostPasscode
+        })
 
+        response = requests.request("POST", url, headers=headers, data=payload)
+        logging.logger.info(response.text)
 
-
-
-class Sec_Meeting:
-    pass
-
-
-#if __name__ == '__main__':
-    cancelMeeting()
-#    startMeeting()
+if __name__ == '__main__':
+#    cancelMeeting()
+    startMeeting()
+#    closeMeeting()
